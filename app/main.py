c96c253343813b23dd6bde0f165f16e3b605235e
Andrii Aleksandrov <=>
2021-07-01 20:58:41 +0300

    Add message broadcasting for simultaneous connections

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import asyncio
 import ast
+from typing import List
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from aiokafka import AIOKafkaConsumer
+from starlette.websockets import WebSocket
 from starlette.endpoints import WebSocketEndpoint
 
 from app.schemas import Message
@@ -38,6 +40,35 @@ html = """
 consumer: AIOKafkaConsumer
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+
+manager = ConnectionManager()
+
+
+async def consume_messages():
+    async for msg in consumer:
+        try:
+            msg_dict = ast.literal_eval(msg.value.decode('UTF-8'))
+            message = Message(**msg_dict)
+            await manager.broadcast(message.json())
+        except:
+            continue
+
+
 @app.on_event('startup')
 async def startup_event():
     global consumer
@@ -45,12 +76,18 @@ async def startup_event():
         *settings.KAFKA_TOPICS,
         bootstrap_servers=f'{settings.KAFKA_HOST}:{settings.KAFKA_PORT}',
     )
+
     await consumer.start()
+    global consume_task
+    consume_task = asyncio.create_task(
+        consume_messages()
+    )
 
 
 @app.on_event('shutdown')
 async def shutdown_event():
     await consumer.stop()
+    consume_task.cancel()
 
 
 @app.get("/")
@@ -62,20 +99,7 @@ async def get():
 class ConsumerApp(WebSocketEndpoint):
 
     async def on_connect(self, websocket):
-        await websocket.accept()
-        self.consume_task = asyncio.create_task(
-            self.consume_messages(websocket)
-        )
+        await manager.connect(websocket)
 
     async def on_disconnect(self, websocket, close_code):
-        await websocket.close()
-        self.consume_task.cancel()
-
-    async def consume_messages(self, websocket):
-        async for msg in consumer:
-            try:
-                msg_dict = ast.literal_eval(msg.value.decode('UTF-8'))
-                message = Message(**msg_dict)
-                await websocket.send_json(message.json())
-            except:
-                continue
+        manager.disconnect(websocket)
